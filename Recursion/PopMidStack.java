6835379873c27401109219e83ca996ccb6258001
VintageCoder <vishwakarma.vivek80@gmail.com>
2022-07-26 17:15:24 +0530

    Recurion Commit 4

---
+++ Recursion/PopMidStack.java
@@ -0,0 +1,40 @@
+package Recursion;
+
+import java.util.Stack;
+
+public class PopMidStack {
+	
+	public static void delete(Stack<Integer> s,int mid,int count ) {
+		
+		int x=s.pop();
+//		System.out.println(s);
+		if (count==mid-1) return;
+		delete(s,mid,count+1);
+		s.push(x);
+	}
+	
+	
+	public static void deleteMid(Stack<Integer>s,int sizeOfStack){
+        // code here
+        int mid=(sizeOfStack/2)+1;
+        int count=1;
+        delete(s, mid, count);
+         
+        
+    } 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack<Integer> s = new Stack<Integer>();
+		s.add(1);
+		s.add(2);
+		s.add(3);
+		s.add(4);
+//		s.add(5);
+//		s.add(23);
+//		Stack<Integer> tmpStack=deleteMid(input);
+		deleteMid(s,6);
+		System.out.println("Deleted Stack: " +s);
+
+	}
+
+}
